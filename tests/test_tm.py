6b4ec325145ff19752c3a1d323a2cf308d8dbae8
Nathan Patnam <patnamnv@email.wofford.edu>
2019-04-28 23:58:22 -0400

    turing machine recognizes transitions

--- tests/test_tm.py
+++ tests/test_tm.py
@@ -4,36 +4,51 @@ import sys
 sys.path.append("..")  # Adds higher directory to python modules path.
 
 
-
-
-
 class Testtm(object):
     @pytest.fixture(scope="function")
     def create_tm(self):
-        tm = TM("set_1/tm.txt")
+        tm = TM("tests/set_1/tm.txt")
         return tm
 
-
     def test_parse_line_for_tm_states(self, create_tm):
-        states = set(["q1", "q2", "q3", "q4" ,"q5","accept","reject"])
+        states = set(["q1", "q2", "q3", "q4", "q5", "accept", "reject"])
         tm_states = create_tm.get_states()
         assert states == tm_states
-    
+
     def test_parse_line_for_tm_input_alphabet(self, create_tm):
         alphabet = set("0")
         tm_alphabet = create_tm.get_input_alphabet()
         assert alphabet == tm_alphabet
-    
+
     def test_parse_line_for_tm_tape_alphabet(self, create_tm):
         tape_alphabet = set(["0", "x", "_"])
         tm_tape_alphabet = create_tm.get_tape_alphabet()
         assert tape_alphabet == tm_tape_alphabet
-    
+
     def test_parse_line_for_tm_start_state(self, create_tm):
         start_state = "q1"
         tm_start_state = create_tm.get_start_state()
         assert start_state == tm_start_state
 
+    def test_parse_lines_for_tm_transitions(sxelf, create_tm):
+        transitions = {
 
-   
-   
+            ("q1", "0"): ("_", 'q2', "R"),
+            ("q1", "_"): ("_", "reject", "R"),
+            ("q1", "x"): ("x", 'reject', 'R'),
+            ("q2", "x"): ('x', "q2", "R"),
+            ("q2", "0"): ("x", "q3", "R"),
+            ("q2", "_"): ("_", "accept", "R"),
+            ("q3", "x"): ("x", 'q3', "R"),
+            ("q3", "_"): ("_", "q5", "L"),
+            ("q3", "0"): ("0", "q4", "R"),
+            ("q4", "x"): ("x", "q4", "R"),
+            ("q4", "0"): ("x", "q3", "R"),
+            ("q4", "_"): ("_", "reject", "R"),
+            ("q5", "0"): ("0", "q5", "L"),
+            ("q5", "x"): ("x", "q5", 'L'),
+            ("q5", "_"): ("_", 'q2', "R"),
+        }
+
+        tm_transitions = create_tm.get_transitions()
+        assert transitions == tm_transitions
--- tm.py
+++ tm.py
@@ -1,4 +1,86 @@
 class TM:
+    def __init__(self, filename):
+        self.filename = filename
+        self.states = set()
+        self.input_alphabet = set()
+        self.tape_alphabet = set()
+        self.start_state = ""
+        self.transitions = {}
+        self.build_TM(filename)
+        
+    
 
-    def __init__(self):
-        self.name = "" 
+    def build_TM(self, filename):
+        tm_fh = open(filename, "r")
+        lines = tm_fh.readlines()
+        line_number = 1
+        for line in lines:
+            if line_number == 1:
+                self.set_tm_states(line)
+            elif line_number == 2:
+                self.set_tm_input_alphabet(line)
+            elif line_number == 3:
+                self.set_tm_tape_alphabet(line)
+            elif line_number == 4:
+                self.set_tm_start_state(line)
+            else:
+                self.add_transition(line)
+            line_number += 1
+    
+
+    def set_tm_states(self, line):
+        self.states = self.convert_comma_seperated_string_to_set(line)
+
+    
+    def set_tm_input_alphabet(self, line):
+        self.input_alphabet = self.convert_comma_seperated_string_to_set(line)
+    
+    def set_tm_tape_alphabet(self, line):
+        self.tape_alphabet = self.convert_comma_seperated_string_to_set(line)
+    
+    def set_tm_start_state(self, line):
+        line = self.remove_whitespace_and_newline(line)
+        self.start_state = line
+    
+    def add_transition(self, line):
+        line = self.remove_whitespace_and_newline(line)
+        items = line.split(",")
+        start_state = items[0]
+        read_character = items[1]
+        write_character = items[2]
+        end_state = items[3]
+        direction_change = items[4]
+
+        self.transitions[(start_state, read_character)] = (write_character, end_state, direction_change)
+
+
+    def convert_comma_seperated_string_to_set(self, line):
+        data_container = set()
+        line = self.remove_whitespace_and_newline(line)
+        items = line.split(",")
+        for item in items:
+            data_container.add(item)
+        return data_container
+
+    def remove_whitespace_and_newline(self, line):
+        return line.strip().replace("\n", "")
+
+
+    def get_states(self):
+        return self.states
+    
+    def get_input_alphabet(self):
+        return self.input_alphabet
+    
+    def get_tape_alphabet(self):
+        return self.tape_alphabet
+    
+    def get_start_state(self):
+        return self.start_state
+    
+    def get_transitions(self):
+        return self.transitions
+    
+    
+
+
